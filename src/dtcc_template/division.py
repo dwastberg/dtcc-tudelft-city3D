11703a603d3a0388cee05e17137be05e9a49e029
Dag Wästberg <dwastberg@gmail.com>
2023-08-28 18:24:00 +0200

    initial commit

--- src/dtcc_template/division.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2023 Vasilis Naserentin
-# Licensed under the MIT License
-
-def div(i, j):
-    return i // j
--- src/dtcc_template/multiplication.py
+++
@@ -1,5 +0,0 @@
-# Copyright (C) 2023 Anders Logg
-# Licensed under the MIT License
-
-def mul(i, j):
-    return i * j
