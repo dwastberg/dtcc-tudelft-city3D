8c47959904d6ad9508660be1b8471026eeffd027
Dag Wästberg <dwastberg@gmail.com>
2023-09-03 13:33:30 +0200

    run test script on Helsingborg

--- test_script.py
+++ test_script.py
@@ -6,14 +6,26 @@ import numpy as np
 city = io.load_city(
     "/Users/dwastberg/repos/dtcc-platform/dtcc-builder/tests/data/MinimalCase/PropertyMap.shp"
 )
+
 pc = io.load_pointcloud(
     "/Users/dwastberg/repos/dtcc-platform/dtcc-builder/tests/data/MinimalCase/PointCloud.las"
 )
 
+city = io.load_city(
+    "/Users/dwastberg/repos/dtcc-platform/dtcc-builder/data/HelsingborgHarbour2022/PropertyMap.shp"
+)
+
+pc = io.load_pointcloud(
+    "/Users/dwastberg/repos/dtcc-platform/dtcc-builder/data/HelsingborgHarbour2022/PointCloud.las"
+)
+
+
 city = builder.city_methods.compute_building_points(city, pc)
-for building in city.buildings:
-    pt_offset = np.zeros_like(building.roofpoints.points)
-    pt_offset[:, 2] = np.random.uniform(0, 0.1, size=pt_offset.shape[0])
-    building.roofpoints.points += pt_offset
 city = builder.city_methods.compute_building_heights(city)
+city = city.terrain_from_pointcloud(pc, 1)
 city = city3d.generate_building_meshes(city)
+
+for idx, building in enumerate(city.buildings):
+    print(f"Building {idx} has {len(building.mesh.vertices)} vertices")
+    print(f"saving building_{idx}.obj")
+    io.save_mesh(building.mesh, f"building_{idx}.obj")
